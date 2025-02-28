414999a3060e113b494ea712d9bb0d83a0749e02
Ellian Carlos <elliancarlos@gmail.com>
2025-02-28 03:47:20 -0300

    Use oxalica overlay instead of cargo2nix to enable use of the latest rust version

--- flake.nix
+++ flake.nix
@@ -2,44 +2,67 @@
   description = "Etanol - A declarative engine for managing orchestration.";
 
   inputs = {
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+    };
+
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; 
+    nixpkgs.follows = "rust-overlay/nixpkgs";
     systems.url = "github:nix-systems/default";
-    cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
     flake-utils = {
         url = "github:numtide/flake-utils";
-        follows = "cargo2nix/flake-utils";
         inputs.systems.follows = "systems";
     };
   };
 
-  outputs = { self, systems, nixpkgs, flake-utils, cargo2nix }:
+  outputs = { self, systems, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let 
-        pkgs = import nixpkgs { inherit system; overlays = [ cargo2nix.overlays.default ]; };
-
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.75.0";
-          packageFun = import ./Cargo.nix;
-          extraRustComponents = [ "clippy" "rustfmt" ];
-        };
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
       in rec {
         packages = {
-          etanol = (rustPkgs.workspace.etanol {});
+          etanol = pkgs.rustPlatform.buildRustPackage {
+            name = "etanol";
+            pname = "etanol";
+            version = "0.0.0";
+            src = self;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [ protobuf ];
+
+            buildPhase = ''
+              cargo build
+            '';
+
+            meta = {
+              description = "Etanol - A declarative engine for managing orchestration.";
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ yourname ];
+            };
+          };
           default = packages.etanol;
         };
 
-        devShells.default = rustPkgs.workspaceShell {
-            buildInputs = with pkgs; [
-              rustc
-              cargo
-              rustfmt
-              clippy
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            rustup default stable
+          '';
 
-              nil
-              nixpkgs-fmt
-              direnv
-            ];
+          buildInputs = with pkgs; [
+            rustup
+            rustc
+            cargo
+            rustfmt
+            clippy
+
+            protobuf
+
+            nil
+            nixpkgs-fmt
+            direnv
+          ];
         };
 
       formatter = pkgs.nixfmt-rfc-style;
